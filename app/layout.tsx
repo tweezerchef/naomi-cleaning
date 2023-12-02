2e7a08b4aca2c69fb91aed94d9ab470f7b87ac72
tweezerchef <ltomblock@gmail.com>
2023-12-02 17:42:23 -0600

    checkoutOldBranch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import Header from '../components/Layout/Header';
